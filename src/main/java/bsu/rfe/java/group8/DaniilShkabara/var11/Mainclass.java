5b6df2ec9e0fc483011626e14b7c6165c4529a16
Daniil <danikshkabara@tut.by>
2020-10-18 21:35:50 +0300

    init & imported

---
+++ src/main/java/bsu/rfe/java/group8/DaniilShkabara/var11/Mainclass.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bsu.rfe.java.group8.DaniilShkabara.var11;
+
+/**
+ *
+ * @author Даня
+ */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class Mainclass {
+    public static void main(String args[]){
+    
+    }
+}
---
+++ src/main/java/bsu/rfe/java/group8/DaniilShkabara/var11/Mainframe.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bsu.rfe.java.group8.DaniilShkabara.var11;
+
+/**
+ *
+ * @author Даня
+ */
+public class Mainframe {
+    
+}
